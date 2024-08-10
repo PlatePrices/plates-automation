66617790d3fe87c2c3089200842f1fc9f37e1ecd
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-08-10 11:20:17 +0400

    added a script to scrape xplate on network

---
+++ src/config/selectors.ts
@@ -0,0 +1,9 @@
+const SELECTORS = {
+  ERROR_MESSAGE_SELECTOR: "div.alert.alert-warning.text-center.m-0.rounded-3",
+  ALL_PLATES: "div.col-6.col-sm-4.my-1.p-1",
+  PLATE_PRICE: "span.custom-red.dm-white",
+  PLATE_DURATION: "div.d-flex.align-items-center.meta > div > span",
+  PLATE_LINK:
+    "div.post.post-grid.rounded.bordered.p-1.h-100.dm-bordered > div > a",
+};
+export default SELECTORS;
